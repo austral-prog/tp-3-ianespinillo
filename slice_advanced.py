60b1d70bb3bc4e5b205767697d910b999299ed82
Ian Espinillo <iespinillo@mail.austral.edu.ar>
2025-04-07 14:43:27 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,5 +1,8 @@
 def slice_advanced():
     # Código a implementar utilizando input.
-
+    texto = input("Ingrese un texto: ")
+    texto = texto.lower()
+    print(texto[4:len(texto):2])  # Se espera que imprima "Awe"
+slice_advanced()
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
